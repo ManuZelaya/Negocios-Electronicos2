c839b74adf1b9060b73c0c5986bce29ba17fffd3
ManuZelaya <78713066+ManuZelaya@users.noreply.github.com>
2022-06-14 10:52:25 -0500

    Pagina de Comercio electronico

---
+++ Catalogo-Feliz/Funciones.js
@@ -0,0 +1,16 @@
+function sumar(a=0, b=0){
+document.getElementById("sumar").innerHTML=a+b;
+}
+document.getElementById("sumar").onclick=function(){
+    let pr1=0;
+    let pr2=0;
+
+    document.getElementById("pr1").checked{
+        pr1=300;
+    };
+    document.getElementById("pr2").checked{
+        pr2=200;
+    };
+    sumar(pr1, pr2)
+    
+}
